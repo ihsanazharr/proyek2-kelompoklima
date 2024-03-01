10297c40294f9cc2e5e39df20e6a6e6144eae5fc
IlhamFaisalR23 <ilhamfaisalrr@gmail.com>
2024-03-01 22:29:44 +0700

    Add Admin

--- disdukcapil.h
+++ disdukcapil.h
@@ -2,3 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
+#define MAX_USERNAME_LENGTH 50
+#define MAX_PASSWORD_LENGTH 50
+
+typedef struct{
+    char username[MAX_USERNAME_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
+    int jumlahGeser;//tambahan subvar untuk jumlah geser & arah geser
+    char arahGeser;
+} Admin;
+
+void addAdmin();
+void enkripsiPassword(char *password, int jumlahGeser);
+void generateAngkaGeser(int *jumlahgeser, char *arahgeser);
+void simpanFileAdmin(Admin admin);
--- main.c
+++ main.c
@@ -1,9 +1,28 @@
 #include "disdukcapil.h"
 #include <stdio.h>
 
-int main()
-{
-	printf("Tes Program");
-	printf("Tes program 2");
-	return 1;
+int main() {
+    int pilihan;
+
+    do {
+        printf("Menu:\n");
+        printf("1. Register Admin\n");
+        printf("2. Keluar\n");
+        printf("Pilih menu: ");
+        scanf("%d", &pilihan);
+
+        switch (pilihan) {
+            case 1:
+                addAdmin();
+                break;
+            case 2:
+                printf("Program keluar\n");
+                break;
+            default:
+                printf("Masukkan tidak valid, coba kembali.\n");
+                break;
+        }
+    } while (pilihan != 2);
+
+    return 0;
 }
