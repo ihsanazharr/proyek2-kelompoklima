aa75f05a6724aa64e876d4a68b154091eea5873d
Firlyansyah <undeadcatgirl5@gmail.com>
2024-03-02 12:53:53 +0700

    Menambahkan prosedur loginAdmin dan dekripsiPassword pada file disdukcapil.c. dan sedikit merubah menu pilihan di main.c

--- main.c
+++ main.c
@@ -1,28 +1,35 @@
 #include "disdukcapil.h"
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int pilihan;
 
-    do {
+    do
+    {
         printf("Menu:\n");
         printf("1. Register Admin\n");
-        printf("2. Keluar\n");
+        printf("2. Login Admin\n");
+        printf("3. Keluar\n");
         printf("Pilih menu: ");
         scanf("%d", &pilihan);
 
-        switch (pilihan) {
-            case 1:
-                addAdmin();
-                break;
-            case 2:
-                printf("Program keluar\n");
-                break;
-            default:
-                printf("Masukkan tidak valid, coba kembali.\n");
-                break;
+        switch (pilihan)
+        {
+        case 1:
+            addAdmin();
+            break;
+        case 2:
+            loginAdmin();
+            break;
+        case 3:
+            printf("Keluar dari program.");
+            break;
+        default:
+            printf("Masukkan tidak valid, coba kembali.\n");
+            break;
         }
-    } while (pilihan != 2);
+    } while (pilihan != 3);
 
     return 0;
 }
